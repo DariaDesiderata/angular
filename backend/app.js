415af4f8786f7c8b6378676b455ab8e56bf5c25e
DariaDesiderata <daria.calwell@gmail.com>
2017-03-31 14:19:58 -0600

    add routes paths to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,25 @@
-require('dotenv').congig();
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
-
-//Route files
-const product = require('./routes/product')
-
 const PORT = process.env.PORT || 3000
+
+// //Route files
+// const farm = require('./routes/farm')
+// const category = require('./routes/category')
+// const product = require('./routes/product')
 
 //Middleware
 app.use(helmet())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-//routes
-app.use('/product', product)
+// //routes
+// app.use('/farm', farm)
+// app.use('/category', category)
+// app.use('/product', product)
 
 //listening PORT
 app.listen(PORT, () => {
